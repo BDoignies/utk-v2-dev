c2893b3dbdcbe5ef1f2f3f072c38eba8b1bedcab
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2023-06-01 18:11:31 +0200

    Remove lloyd3D compilation on windows !

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     python_requires=">=3.6",
     install_requires=[
         'numpy'
+        # TODO : install GCC ?
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
